e8073db4d377db28791a50c35a48ee5acf502524
PritamJogi <pritamjogi09@gmail.com>
2024-10-09 18:01:35 +0530

    final update

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const {HoldingModel}= require("./Models/HoldingModel");
 const { PositionModel } = require('./Models/PositionModel');
 const app = express();
+const {OrderModel} = require("./Models/OrderModel");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,6 +22,17 @@ app.get("/allHolding", async(req,res)=>{
 app.get("/allposition",async(req,res)=>{
     let allposition = await PositionModel.find({});
     res.json(allposition);
+});
+
+app.post("/newOrder",async(req,res)=>{
+    let newOrder = new OrderModel({
+        name: req.body.name,
+        qty:req.body.qty,
+        price: req.body.price,
+        mode: req.body.mode,
+    });
+    newOrder.save();
+    res.send("order saved");
 });
 
 app.listen(PORT,()=>{
